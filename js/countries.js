a793545af7ff4300d3f0762885446c9d62d3c48b
CoderMahabub <codermahabub@gmail.com>
2021-08-27 02:04:17 +0600

    33-4 (advanced) Call dynamic API, load dynamic data to display

--- js/countries.js
+++ js/countries.js
@@ -13,15 +13,36 @@ const displayCountries = (countries) => {
     // As they provide countries info as an array, we can use forEach to do the same work;
     const countriesContainer = document.getElementById('countries');
     countries.forEach(country => {
-        console.log(country);
+        // console.log(country);
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.name;
+        // div.appendChild(h3);
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
+        // div.appendChild(p);
+        div.innerHTML = `
+        <h3>${country.name}</h3>
+        <p>${country.capital}</p>
+        <button onclick="loadCountryByName('${country.name}')">Details</button>
+        `;
         countriesContainer.appendChild(div);
     });
+}
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]))
+}
+
+const displayCountryDetail = country => {
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+    <h4>${country.name}</h4>
+    <p>Population: ${country.population}</p>
+    <img height="auto" width="200px" src="${country.flag}">
+    `;
 }
