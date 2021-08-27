a2213948a51bdced9e51c6b2b14a2c377227db03
CoderMahabub <codermahabub@gmail.com>
2021-08-27 21:37:06 +0600

    33-6 Display dynamic search result using bootstrap cards

--- js/mealdb.js
+++ js/mealdb.js
@@ -8,5 +8,26 @@ const searchFood = () => {
     // console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displaySearchResult(data.meals));
+}
+
+const displaySearchResult = (meals) => {
+    console.log(meals);
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="col">
+            <div class="card">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions.slice(0, 300)}</p>
+                </div>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
