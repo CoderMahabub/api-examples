cca5940362fb3279192e4113e47f837b15284867
CoderMahabub <codermahabub@gmail.com>
2021-08-28 00:45:12 +0600

    33-7 Create dynamic url based on click and display data

--- js/mealdb.js
+++ js/mealdb.js
@@ -12,13 +12,13 @@ const searchFood = () => {
 }
 
 const displaySearchResult = (meals) => {
-    console.log(meals);
+    // console.log(meals);
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="col">
+        <div onclick="loadMealDetail('${meal.idMeal}')" class="col">
             <div class="card">
                 <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -30,4 +30,30 @@ const displaySearchResult = (meals) => {
         `;
         searchResult.appendChild(div);
     })
+}
+
+const loadMealDetail = mealId => {
+    // console.log(mealId)
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetail(data.meals[0]))
+}
+
+const displayMealDetail = meal => {
+    // console.log(meal);
+    const mealDetails = document.getElementById('meal-detail');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <div class="card">
+            <img width="100px" src="${meal.strMealThumb}" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p> ${meal.strInstructions.slice(0, 300)}</p>
+                <a href="${meal.strYoutube}" class="btn btn-primary">Video Link</a>
+            </div>
+        </div>
+    `;
+    mealDetails.appendChild(div);
 }
